c19aad8627c231636455eb611b9db38987926286
apradeep <apradeep@DESKTOP-P2U99H5>
2023-09-13 18:16:26 -0600

    Changed the URL

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,7 +2,7 @@ import requests
 
 print(requests.__version__)
 
-resp = requests.get("https://www.google.com")
+resp = requests.get("https://raw.githubusercontent.com/anujpradeep/CMPUT404_Lab1/master/Lab1/lab1.py")
 
 
 print("Status Code is {}".format(resp.status_code))
